f1159f3c84cd60e1123713b1055c1dce23cfb50c
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2021-07-23 11:58:34 +0530

    .

--- src/flask_app.py
+++ src/flask_app.py
@@ -1,13 +1,29 @@
 import secrets
 from flask import (
     Flask,
+    render_template,
 )
 
 import skrun
 
+import os
+import shutil
+import sys
 
 app = Flask(__name__)
 
-@app.route('/')
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/.."
+sys.path.insert(1, THIS_FOLDER)
+
+shutil.rmtree("sessions", ignore_errors=True)
+os.system("mkdir sessions")
+
+sessions = {}
+terminated = set()
+
+@app.route('/', methods=("POST", "GET"))
 def home():
-    return "Hello, World!"
+    session = secrets.token_hex(64)
+    sessions[session] = None
+    return render_template("index.html")
+
