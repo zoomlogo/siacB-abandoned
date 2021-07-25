a4e67019234096d90e1a32cabb11603215dd7658
pygamer0 <maanasyt@gmail.com>
2021-07-25 12:42:33 +0530

    my head is hurting

--- src/flask_app.py
+++ src/flask_app.py
@@ -1,4 +1,5 @@
 import secrets
+import multiprocessing
 from flask import (
     Flask,
     render_template,
@@ -31,10 +32,50 @@ def home():
 
 @app.route('/execute', methods=("POST",))
 def execute():
-    flags = request.form["flags"].split("\n")
+    flags = request.form["flags"].split('\n')
     code = request.form["code"]
-    stdin = request.form["stdin"].split("\n")
+    stdin = request.form["stdin"].split('\n')
     session = request.form["session"]
 
-    print(flags, code, stdin, session)
+    if session not in sessions:
+        return {
+            "stdout": "",
+            "stderr": "The session was invalid! You may need to reload your tab.",
+        }
+
+    shutil.rmtree(f"sessions/{session}", ignore_errors=True)
+    os.mkdir(f"sessions/{session}")
+
+    with open(f"sessions/{session}/.stderr", "w", encoding='utf-8') as stderr:
+        manager = multiprocessing.Manager()
+        ret = manager.dict()
+
+        time = 15
+        if "T" in flags:
+            time = 60
+        ret[1] = ""
+        ret[2] = ""
+
+        sessions[session] = multiprocessing.Process(
+            target=skrun,
+            args=(code, flags, stdin),
+        )
+        sessions[session].start()
+        sessions[session].join(time)
+
+        if session in terminated:
+            terminated.remove(session)
+            ret[2] += "\nSession terminated upon user request"
+        if sessions[session].is_alive():
+            sessions[session].kill()
+            if 2 in ret:
+                ret[2] += "\n" + f"Code timed out after {time} seconds"
+        stderr.write(ret[2])
+        print(ret[1])
+
+    result = {
+        "stdout": None,
+        "stderr": None,
+    }
+    return result
 
